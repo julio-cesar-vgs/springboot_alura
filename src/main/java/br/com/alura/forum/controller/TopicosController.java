fadef87898b8cf1483d6912994087bed060035b2
Sleepk <julio.cesar.vgs@gmail.com>
2020-04-15 12:32:48 -0300

    Spring boot parte 1 - Capitulo 3 - Aula - 2 Utilizando Repository

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,19 +3,24 @@ package br.com.alura.forum.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
 
 @RestController
 public class TopicosController {
+	
+	@Autowired
+	private TopicoRepository topicoRepository;
+	
 	@RequestMapping("/topicos")
 	public List<TopicoDto> lista() {
-
-		Topico topico = new Topico("Duvida", "Erro spring", new Curso("Spring", "Java Web"));
-		return TopicoDto.converter(Arrays.asList(topico));
+	List<Topico> topicos = topicoRepository.findAll();
+		return TopicoDto.converter(topicos);
 	}
 }
