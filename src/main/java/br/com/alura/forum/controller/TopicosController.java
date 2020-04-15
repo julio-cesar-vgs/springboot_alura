74a3d8b552675d49c24e62b72c512c25ce7b8bd7
Sleepk <julio.cesar.vgs@gmail.com>
2020-04-14 23:04:35 -0300

    Spring boot parte 1 - Capitulo 2 - Aula - 3 Usando DTO

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,20 +3,19 @@ package br.com.alura.forum.controller;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
 
-@Controller
+@RestController
 public class TopicosController {
 	@RequestMapping("/topicos")
-	@ResponseBody
-	public List<Topico> lista() {
+	public List<TopicoDto> lista() {
 
 		Topico topico = new Topico("Duvida", "Erro spring", new Curso("Spring", "Java Web"));
-		return Arrays.asList(topico);
+		return TopicoDto.converter(Arrays.asList(topico));
 	}
 }
