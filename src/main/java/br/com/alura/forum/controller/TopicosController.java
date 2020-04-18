784d76f40cc923e16d475c480b8db07aafef3d31
Sleepk <julio.cesar.vgs@gmail.com>
2020-04-18 11:14:07 -0300

    Spring boot parte 1 - Capitulo 3 - Aula - 3 - Consulta com filtros

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,6 +1,5 @@
 package br.com.alura.forum.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
-import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.repository.TopicoRepository;
 
@@ -19,8 +17,14 @@ public class TopicosController {
 	private TopicoRepository topicoRepository;
 	
 	@RequestMapping("/topicos")
-	public List<TopicoDto> lista() {
-	List<Topico> topicos = topicoRepository.findAll();
-		return TopicoDto.converter(topicos);
+	public List<TopicoDto> lista(String nomeCurso) {
+		if(nomeCurso == null) {
+			List<Topico> topicos = topicoRepository.findAll();
+			return TopicoDto.converter(topicos);
+		}else {
+			List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+			return TopicoDto.converter(topicos);
+
+		}
 	}
 }
